6966c524acf91d160fc03cd2a40656e2e20e045b
jk-jumur <developer.jumur@gmail.com>
2026-07-13 02:05:27 +0600

    recap and design grid layout , fetch categories

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,13 +1,39 @@
-import Image from "next/image";
+async function getCategories() {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/news/categories"
+  );
 
-export default function Home() {
+  const data = await res.json();
+  return data.data;
+}
+
+export default async function Home() {
+  const categories = await getCategories();
+
+  console.log(categories.news_category);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="container mx-auto grid grid-cols-12 gap-4 my-[60px]">
+      <div className="col-span-3  ">
+         <h2 className="font-bold text-lg"> All Categories</h2>
        
-        
-       
-      </main>
+
+       <ul className="flex flex-col gap-4">
+           {
+           categories.news_category.map(category => {
+            return <li key={category.category_id} className="text-center font-bold   rounded-md bg-slate-100 p-2 text-[#9F9F9F]">{category.category_name}</li>
+           })
+        }
+       </ul>
+      </div>
+
+      <div className="col-span-6 bg-pink-600 font-bold">
+        Dragon News Home
+      </div>
+
+      <div className="col-span-3 bg-amber-400 font-bold">
+        Login With
+      </div>
     </div>
   );
 }
